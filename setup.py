4ebdcf588e86f7adc736499285aec2323136527f
robes <robes@users.noreply.github.com>
2012-09-07 01:28:12 +0000

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name='tagfiler-iobox',
+      version='0.1',
+      package_dir={'tagfiler': 'src/tagfiler'},
+      packages=['tagfiler', 'tagfiler.iobox', 'tagfiler.util'],
+      )
