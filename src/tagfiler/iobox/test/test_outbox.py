857b0572e52df2568abfe3c6c5a18a41fc328d57
robes <robes@users.noreply.github.com>
2012-09-10 21:39:47 +0000

    Fixed bug in worker and improved tests

--- src/tagfiler/iobox/test/test_outbox.py
+++ src/tagfiler/iobox/test/test_outbox.py
@@ -3,41 +3,72 @@ Placeholder for a proper Python unittest module.
 '''
 
 import logging
-from tagfiler.iobox.worker import WorkQueue, Worker
-from tagfiler.iobox import find, tag, register
+import tagfiler.iobox.worker as worker
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+
+class TestWorker(worker.Worker):
+    '''A test worker.
+    
+    The test worker simulates a stage in a worker thread pipeline. It takes a
+    task queue for inputs to the stage and a resutls queue as the output for
+    the stage. It takes a worker_id to add to print in the log. And it
+    generates a (results_per_task) number of dummy results for every task it
+    takes off the intput tasks queue.
+    '''
+    
+    def __init__(self, tasks, results, worker_id, results_per_task):
+        '''Initializes the Worker class.
+        
+        Arguments:
+        tasks -- a WorkQueue of tasks.
+        results -- a WorkQueue of results.
+        worker_id -- an identifier for this test worker.
+        generate_num_tasks -- the number of results per task to produce. 
+        '''
+        worker.Worker.__init__(self, tasks, results)
+        self.results_per_task = results_per_task
+        self.worker_id = worker_id
+    
+    def do_work(self, task, work_done):
+        logger.debug('Stage(%d) Task: %d' % (self.worker_id, task))
+        for i in range(self.results_per_task):
+            result = i
+            work_done(result)
+        return
+
 
 if __name__ == '__main__':
     
     logger.info('TEST BEGIN')
     
-    # Create Find worker tasks
-    find_tasks = WorkQueue()
-    for i in range(3):
-        find_tasks.put(i)
-    find_tasks.put(Worker.DONE)
+    # Create task queues for the pipeline.
+    task_queue_1 = worker.WorkQueue()
+    task_queue_2 = worker.WorkQueue()
+    task_queue_3 = worker.WorkQueue()
 
-    # Create Tag worker tasks    
-    tag_tasks = WorkQueue()
-    
-    # Create Register worker tasks
-    register_tasks = WorkQueue()
-    
-    final = WorkQueue()
-    
-    f = find.Find(find_tasks, tag_tasks)
-    t = tag.Tag(tag_tasks, register_tasks)
-    r = register.Register(register_tasks, final)
-    r.start()
-    t.start()
-    f.start()
+    # Populate the first queue with tasks, the other queues will get
+    # filled by the the pipelined workers.
+    for i in range(10):
+        task_queue_1.put(i)
+    task_queue_1.put(worker.Worker.DONE)
 
-    # The following join calls are not working as expected... TBD!
-    #find_tasks.join()
-    #tag_tasks.join()
-    #register_tasks.join()
-    #final.join()
+    # Create the test workers
+    stage1 = TestWorker(task_queue_1, task_queue_2, 1, 10)
+    stage2 = TestWorker(task_queue_2, task_queue_3, 2, 5)
+    stage3 = TestWorker(task_queue_3, worker.WorkQueue(), 3, 5)
+
+    # Start them in reverse order so they are waiting for stage 1 to start
+    # producing tasks to propagate throught the pipeline.
+    stage3.start()
+    stage2.start()
+    stage1.start()
+
+    # Now wait/join on the task queues so that the main thread will not exit
+    # before the workers have finished.
+    task_queue_1.join()
+    task_queue_2.join()
+    task_queue_3.join()
 
     logger.info('TEST END')
--- src/tagfiler/iobox/worker.py
+++ src/tagfiler/iobox/worker.py
@@ -1,8 +1,5 @@
 '''
-This module implements a simple worker thread.
-
-It is not yet complete or entirely correct. The idea is to implement a 
-threaded pipeline.
+A simple worker thread module, intended for use in a threaded pipeline.
 '''
 
 import logging
@@ -12,16 +9,34 @@ import Queue
 logger = logging.getLogger(__name__)
 
 class WorkQueue(Queue.Queue):
-    '''A simple work queue implementation.'''
+    '''A simple work queue.
     
-    # We probably want to hid the Queue implementation by wrapping a Queue 
+    Right now it does nothing more than the underlying Queue.Queue class. This
+    implementatoin should be replaced with a composition of rather than a
+    subclass of the Queue.Queue.
+    '''
+    
+    # We probably want to hide the Queue implementation by wrapping a Queue 
     # instance in a WorkQueue so that we can control the methods exposed to
     # the caller. For now, we pass...
     pass
 
 class Worker(threading.Thread):
-    '''Generic worker thread class for IO Box operations.'''
+    '''A simple worker thread.
     
+    The worker thread takes a task queue and a results queue as parameters to
+    its initializer. The worker processes the tasks one-by-one until it reaches
+    the DONE marker task. For each task, it may produce 0-N results which are
+    put on the results queue. If no task is available on the task queue, it
+    blocks for the next available task. When it processed the DONE marker, it
+    terminates.
+    
+    We ought to add a terminate() call and reimplement the blocking behavior so
+    that the thread can be terminated out of sequence.
+    '''
+
+    # The marker for the end of the work queue. Maybe this should be moved over
+    # to the WorkQueue class.
     DONE = 'tagfiler.iobox.worker.Worker.DONE'
         
     def __init__(self, tasks, results):
@@ -58,10 +73,11 @@ class Worker(threading.Thread):
         logger.debug('run:BEGIN')
 
         task = self.tasks.get()
+        self.tasks.task_done()
         while task != Worker.DONE:
             self.do_work(task, self._work_done)
-            self.tasks.task_done()
             task = self.tasks.get()
+            self.tasks.task_done()
 
         self.results.put(Worker.DONE)
 
