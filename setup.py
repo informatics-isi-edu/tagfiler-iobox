292b732faa410661d2edcfd4da7db16919938c2f
robes <robes@users.noreply.github.com>
2012-09-10 21:39:21 +0000

    Added tagfiler.tagfiler-iobox.common package

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 setup(name='tagfiler-iobox',
       version='0.1',
       package_dir={'tagfiler': 'src/tagfiler'},
-      packages=['tagfiler', 'tagfiler.iobox', 'tagfiler.util'],
+      packages=['tagfiler', 'tagfiler.iobox', 'tagfiler.util', 'tagfiler.common'],
       )
