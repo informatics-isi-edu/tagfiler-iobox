579442b658ad700dd73f210d41d5e4f950751058
robes <robes@users.noreply.github.com>
2012-09-19 04:34:39 +0000

    Updated Outbox per changes in Tag and Register

--- src/tagfiler/iobox/outbox.py
+++ src/tagfiler/iobox/outbox.py
@@ -14,30 +14,32 @@
 # limitations under the License.
 #
 """
-Management interface representing each Outbox.
+Implements Outbox management.
 """
 
+import worker, find, tag, register, dao, models
+
 import logging
-from tagfiler.iobox import worker, find, tag, register
+
 
 logger = logging.getLogger(__name__)
 
-# It is very likely that this DOES NOT need to be a Thread. It just needs to
-# manage the pipeline of threads and/or threadpools that are doing the work.
-# One argument for keeping this threaded would be that the outbox might need
-# to be event-driven. But that's not certain right now.
+
 class Outbox():
-    """
-    The class that represents each Outbox.
-    
-    It is responsible for initializing each outbox's threaded pipeline. It
-    provides management interfaces to start and terminate the pipeline.
-    """
+    """This class represents each Outbox and manages its operations."""
     
     def __init__(self, outbox_model, state_dao):
-        """Initializes the Outbox according to the required 'outbox_model'
-        parameter of type 'models.Outbox'."""
+        """Initializes the Outbox.
+        
+        The 'outbox_model' parameter is an instance of models.Outbox.
+        
+        The 'state_dao' parameter is an instance of dao.OutboxStateDAO.
+        """
         logger.debug("Outbox:__init__")
+        
+        assert isinstance(outbox_model, models.Outbox)
+        assert isinstance(state_dao, dao.OutboxStateDAO)
+        
         self._model = outbox_model
         self._state_dao = state_dao
         self._terminated = False
@@ -53,36 +55,45 @@ class Outbox():
         # The pipeline consists of the Find, Tag, and Register workers with their
         # associated WorkQueues.
         self._find = find.Find(self._find_q, self._tag_q, 
+                               self._state_dao,
                                self._model.get_inclusion_patterns(),
                                self._model.get_exclusion_patterns())
-        self._tag = tag.Tag(self._tag_q, self._register_q)
+        self._tag = tag.Tag(self._tag_q, self._register_q, 
+                            self._model.get_path_rules()) #TODO(schuler): TBD to verify whether this is the correct parameter for Tag init
         self._register = register.Register(
                                     self._register_q, worker.WorkQueue(),
-                                    self._state_dao, 
                                     self._model.get_tagfiler())
         
+        
+    def start(self):
+        """""Starts the Outbox."""
+        logger.debug("Outbox:start")
+        
+        assert self._terminated != True
+        
+        self._register.start()
+        self._tag.start()
+        self._find.start()
 
     def terminate(self):
-        """Flags the outbox to terminate gracefully."""
+        """Flags the Outbox to terminate gracefully."""
         logger.debug("Outbox:terminate")
+        
+        assert self._terminated != True
+        
         self._terminated = True
         self._find.terminate()
         self._tag.terminate()
         self._register.terminate()
         
     def is_terminated(self):
-        """Indicates whether the outbox (and its pipeline) have terminated cleanly."""
-        return not (self._find.is_alive() or self._tag.is_alive() or self._register.is_alive())
-        
-    def start(self):
-        """""Starts the outbox pipeline."""
-        logger.debug("Outbox:start")
-        self._register.start()
-        self._tag.start()
-        self._find.start()
+        """Indicates whether the Outbox has terminated."""
+        return not (self._find.is_alive() or 
+                    self._tag.is_alive() or 
+                    self._register.is_alive())
         
     def join(self):
-        """Q&D thread synchronized termination for now, but need to really do this later"""
+        """QuickNDirty thread synchronization. TODO: need to re-do this later."""
         self._find_q.join()
         self._tag_q.join()
         self._register_q.join()
