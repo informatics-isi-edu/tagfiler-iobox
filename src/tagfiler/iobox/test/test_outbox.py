aeea9365ab40a9d06f17b0eb8479d3f85e9bed38
robes <robes@users.noreply.github.com>
2012-09-19 04:44:00 +0000

    Refactored test_outbox to use the base test module

--- src/tagfiler/iobox/test/test_outbox.py
+++ src/tagfiler/iobox/test/test_outbox.py
@@ -17,60 +17,27 @@
 Unit tests for the outbox module.
 """
 
-from tagfiler.iobox.test.test_find import create_temp_dirtree, remove_temp_dirtree
-from tagfiler.iobox.cmdline import create_temp_outbox_dao, remove_temp_outbox_dao
-from tagfiler.iobox import models, outbox
+import base
+from tagfiler.iobox import outbox
 
 import unittest
 import logging
 import time
 
+
 logger = logging.getLogger(__name__)
 
 
-class OutboxTest(unittest.TestCase):
-
-    __OUTBOX_NAME   =  'temp_outbox'
-    __NUMROOTS      =  1
-    __NUMDIRS       =  1
-    __NUMFILES      =  10
+class OutboxManagerTest(base.OutboxBaseTestCase):
+    """Test of the Outbox Manager."""
     
-    def setUp(self):
-        # Create the test directory tree
-        self.rootdirs = create_temp_dirtree(OutboxTest.__NUMROOTS, 
-                                            OutboxTest.__NUMDIRS, 
-                                            OutboxTest.__NUMFILES)
-        
-        # Create the temporary OutboxDAO and Outbox
-        p = {'outbox_name':OutboxTest.__OUTBOX_NAME, 'tagfiler_url':'https://host:port/tagfiler', 'tagfiler_username':'username', 'tagfiler_password':'password'}
-        (self.outbox_path, self.outbox_dao) = create_temp_outbox_dao()
-        self.outbox_model = models.Outbox(**p)
-        self.outbox_model.set_name(OutboxTest.__OUTBOX_NAME)
-        self.outbox_model = self.outbox_dao.add_outbox(self.outbox_model)
-        self.state_dao = self.outbox_dao.get_state_dao(self.outbox_model)
-        
-        # Add the roots to the Outbox model object
-        for rootdir in self.rootdirs:
-            root = models.Root()
-            root.set_filepath(rootdir)
-            self.outbox_dao.add_root_to_outbox(self.outbox_model, root)
-    
-    def tearDown(self):
-        # Remove the test directory tree
-        remove_temp_dirtree(self.rootdirs)
-        
-        # Remove the temporary OutboxDAO
-        remove_temp_outbox_dao(self.outbox_path, self.outbox_dao)
-
-    def testBaseline(self):
-        """Simple test for the Outbox."""
+    def runTest(self):
         outbox_worker = outbox.Outbox(self.outbox_model, self.state_dao)
         outbox_worker.start()
         outbox_worker.join() # TODO: Fix this Q&D synchronization!
         outbox_worker.terminate()
         time.sleep(1)
         self.assertTrue(outbox_worker.is_terminated(), 'Outbox has not terminated')
-        logger.debug("testOutbox: done")
 
 
 if __name__ == "__main__":
