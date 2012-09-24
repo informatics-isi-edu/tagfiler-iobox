a6064dee839882fa0b8f013ccc02cd98110d7b78
robes <robes@users.noreply.github.com>
2012-09-24 22:57:44 +0000

    ch-ch-ch-changes...

--- src/tagfiler/iobox/ui/outbox.py
+++ src/tagfiler/iobox/ui/outbox.py
@@ -5,22 +5,40 @@ sip.setapi('QVariant', 2)
 
 from PyQt4 import QtCore, QtGui
 
-class SystemTrayIcon(QtGui.QSystemTrayIcon):
 
-    def __init__(self, icon, parent=None):
-        QtGui.QSystemTrayIcon.__init__(self, icon, parent)
-        menu = QtGui.QMenu(parent)
-        exitAction = menu.addAction("Exit")
-        self.setContextMenu(menu)
+class OutboxUI():
+    
+    def __init__(self):
+        self.widget = QtGui.QWidget()
+        self.createActions()
+        self.createTrayIcon()
+        
+    def createActions(self):
+        self.quitAction = QtGui.QAction("&Quit", self.widget, triggered=QtGui.qApp.quit)
+        
+    def createTrayIcon(self):
+        # Create tray icon menu
+        self.trayIconMenu = QtGui.QMenu(self.widget)
+        self.trayIconMenu.addAction(self.quitAction)
+        # Create tray icon
+        self.trayIcon = QtGui.QSystemTrayIcon(QtGui.QIcon("tag.gif"), self.widget)
+        self.trayIcon.setContextMenu(self.trayIconMenu)
+        
+    def show(self):
+#        self.widget.show()
+        self.trayIcon.show()
+
 
 def main():
     app = QtGui.QApplication(sys.argv)
     QtGui.QApplication.setQuitOnLastWindowClosed(False)
     
-    w = QtGui.QWidget()
-    trayIcon = SystemTrayIcon(QtGui.QIcon("tag.gif"), w)
-
-    trayIcon.show()
+    outbox = OutboxUI()
+    outbox.show()
+#    w = QtGui.QWidget()
+#    trayIcon = SystemTrayIcon(QtGui.QIcon("tag.gif"), w)
+#    trayIcon.show()
+    
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
