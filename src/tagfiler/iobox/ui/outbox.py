b42a22a64bcaaf5bac3a2c780946d55026f07fe2
robes <robes@users.noreply.github.com>
2012-09-26 21:37:13 +0000

    basic demonstrable functionality

--- src/tagfiler/iobox/ui/outbox.py
+++ src/tagfiler/iobox/ui/outbox.py
@@ -25,7 +25,43 @@ import sys
 import os
 import logging
 import time
+import cStringIO
 
+
+# Ugly and convoluted, but its a work-in-progress
+# TODO: this should be cleaned up later
+logger = logging.getLogger(__name__)
+console_out = cStringIO.StringIO()
+logger.addHandler(logging.StreamHandler(console_out))
+sys.stderr = console_out
+logging.basicConfig(level=logging.DEBUG)
+
+
+class ConsoleWindow(QtGui.QWidget):
+    
+    def __init__(self):
+        super(ConsoleWindow, self).__init__()
+
+        self.setupGui()
+        self.setWindowTitle("Console Messages")
+        
+    def close(self):
+        self.hide()
+
+    def setupGui(self):
+        close_button = QtGui.QPushButton("C&lose")
+        close_button.clicked.connect(self.close)
+
+        bottomLayout = QtGui.QHBoxLayout()
+        bottomLayout.addWidget(close_button)
+
+        self.text_edit = QtGui.QTextEdit()
+
+        mainLayout = QtGui.QVBoxLayout()
+        mainLayout.addWidget(self.text_edit)
+        mainLayout.addLayout(bottomLayout)
+
+        self.setLayout(mainLayout)
 
 class OutboxTrayIconView():
     """The view of the Outbox system tray icon.
@@ -49,17 +85,19 @@ class OutboxTrayIconView():
         # Create context menu and menu actions
         menu = QtGui.QMenu(parent)
         
-        self.configure_action = QtGui.QAction("&Configure", menu)
-        self.console_action = QtGui.QAction("Consol&e", menu)
+        self.preferences_action = QtGui.QAction("&Preferences...", menu)
+        self.help_action = QtGui.QAction("&Help", menu)
+        self.console_action = QtGui.QAction("Consol&e Messages", menu)
         self.start_action = QtGui.QAction("St&art", menu)
         self.stop_action = QtGui.QAction("St&op", menu)
         self.stop_action.setEnabled(False)
-        self.quit_action = QtGui.QAction("&Quit", menu)
+        self.quit_action = QtGui.QAction("&Quit Outbox", menu)
         
         menu.addAction(self.start_action)
         menu.addAction(self.stop_action)
         menu.addSeparator()
-        menu.addAction(self.configure_action)
+        menu.addAction(self.preferences_action)
+        menu.addAction(self.help_action)
         menu.addAction(self.console_action)
         menu.addSeparator()
         menu.addAction(self.quit_action)
@@ -75,80 +113,88 @@ class OutboxTrayIconController():
     """The controller of the Outbox system tray icon.
     """
     
-    def __init__(self, outbox_manager, outbox_model):
+    def __init__(self, outbox_dao, outbox_model):
         """Initialize the object.
         
-        The 'outbox_manager' is required.
+        The 'outbox_dao' is required.
         
         The 'outbox_model' is required.
         """
-        assert isinstance(outbox_manager, outbox.Outbox)
+        assert isinstance(outbox_dao, dao.OutboxDAO)
         assert isinstance(outbox_model, models.Outbox)
-        self.outbox_manager = outbox_manager
+        self.outbox_dao = outbox_dao
         self.outbox_model = outbox_model
+        self.console_window = ConsoleWindow()
+        self.outbox_manager = None
         
-    def configure(self):
+    def preferences(self):
         print 'time to configure'
         self.start_action.setEnabled(True)
         
     def console(self):
-        print 'console'
+        self.console_window.text_edit.clear()
+        self.console_window.text_edit.append(console_out.getvalue())
+        self.console_window.show()
 
     def start(self):
-        print 'time to start'
+        logger.info("Starting Outbox.")
         self.start_action.setEnabled(False)
         self.stop_action.setEnabled(True)
+        self.preferences_action.setEnabled(False)
+        self.state_dao = self.outbox_dao.get_state_dao(self.outbox_model)
+        self.outbox_manager = outbox.Outbox(self.outbox_model, self.state_dao)
         self.outbox_manager.start()
         
     def stop(self):
-        print 'time to stop'
-        self.start_action.setEnabled(True)
-        self.stop_action.setEnabled(False)
+        logger.info("Stopping Outbox. This may take a few moments.")
         self.outbox_manager.terminate()
         while not self.outbox_manager.is_terminated():
-            print 'waiting for outbox to terminate'
             time.sleep(1)
+        logger.info("Outbox stopped.")
+        self.start_action.setEnabled(True)
+        self.stop_action.setEnabled(False)
+        self.preferences_action.setEnabled(True)
             
     def quit(self):
-        print "quitin' time!"
+        if self.outbox_manager and not self.outbox_manager.is_terminated():
+            self.stop()
+        logger.info('Goodbye')
         QtGui.qApp.quit()
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
+    logger.setLevel(logging.DEBUG)
+    logger.info('Welcome to Tagfiler Outbox.')
 
     app = QtGui.QApplication(sys.argv)
     app.setApplicationName('Tagfiler Outbox')
     QtGui.QApplication.setQuitOnLastWindowClosed(False)
     
-    if QtGui.QSystemTrayIcon.isSystemTrayAvailable():
-        print 'system tray available'
-    else:
-        print 'system tray not available'
+    if not QtGui.QSystemTrayIcon.isSystemTrayAvailable():
+        logger.critical('Your system does not support the desktop system tray.')
         sys.exit(1)
     
-    if QtGui.QSystemTrayIcon.supportsMessages():
-        print 'system supports balloon messages'
+    if not QtGui.QSystemTrayIcon.supportsMessages():
+        logger.warning('Your system does not support messages.')
     
-    # Load or create outbox model
-    # TODO: Process some commandline arguments
     default_name = 'default'
     default_path = os.path.join(os.path.expanduser('~'), 
                                 '.tagfiler', 'outbox.conf')
-    (outbox_dao, outbox_model) = config.load_or_create_outbox(default_name, default_path)
-    state_dao = outbox_dao.get_state_dao(outbox_model)
-    outbox_manager = outbox.Outbox(outbox_model, state_dao)
-    
+    logger.info('Loading configuration from %s.' % default_path)
+    (outbox_dao, outbox_model) = \
+        config.load_or_create_outbox(default_name, default_path)
+        
     # Create the tray icon view and controller
-    controller = OutboxTrayIconController(outbox_manager, outbox_model)
     trayicon = OutboxTrayIconView(QtGui.QIcon("tag.gif"))
-    trayicon.configure_action.triggered.connect(controller.configure)
+    controller = OutboxTrayIconController(outbox_dao, outbox_model)
+    controller.start_action = trayicon.start_action
+    controller.stop_action = trayicon.stop_action
+    controller.preferences_action = trayicon.preferences_action
+    trayicon.preferences_action.triggered.connect(controller.preferences)
     trayicon.console_action.triggered.connect(controller.console)
     trayicon.start_action.triggered.connect(controller.start)
     trayicon.stop_action.triggered.connect(controller.stop)
     trayicon.quit_action.triggered.connect(controller.quit)
-    controller.start_action = trayicon.start_action
-    controller.stop_action = trayicon.stop_action
     trayicon.show()
     
     sys.exit(app.exec_())
