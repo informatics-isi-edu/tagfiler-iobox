797d7e402262f0cb820ff479eb978f3fc7429301
robes <robes@users.noreply.github.com>
2012-09-26 00:59:41 +0000

    testing out real outbox functionality

--- src/tagfiler/iobox/ui/outbox.py
+++ src/tagfiler/iobox/ui/outbox.py
@@ -17,12 +17,14 @@
 Experimental desktop GUI client for the Tagfiler Outbox.
 """
 
-from tagfiler.iobox import config, dao, models
+from tagfiler.iobox import config, dao, models, outbox
 
 from PyQt4 import QtCore, QtGui
 
 import sys
 import os
+import logging
+import time
 
 
 class OutboxTrayIconView():
@@ -48,15 +50,17 @@ class OutboxTrayIconView():
         menu = QtGui.QMenu(parent)
         
         self.configure_action = QtGui.QAction("&Configure", menu)
+        self.console_action = QtGui.QAction("Consol&e", menu)
         self.start_action = QtGui.QAction("St&art", menu)
-        self.start_action.setEnabled(False)
         self.stop_action = QtGui.QAction("St&op", menu)
         self.stop_action.setEnabled(False)
         self.quit_action = QtGui.QAction("&Quit", menu)
         
-        menu.addAction(self.configure_action)
         menu.addAction(self.start_action)
         menu.addAction(self.stop_action)
+        menu.addSeparator()
+        menu.addAction(self.configure_action)
+        menu.addAction(self.console_action)
         menu.addSeparator()
         menu.addAction(self.quit_action)
         
@@ -71,27 +75,50 @@ class OutboxTrayIconController():
     """The controller of the Outbox system tray icon.
     """
     
+    def __init__(self, outbox_manager, outbox_model):
+        """Initialize the object.
+        
+        The 'outbox_manager' is required.
+        
+        The 'outbox_model' is required.
+        """
+        assert isinstance(outbox_manager, outbox.Outbox)
+        assert isinstance(outbox_model, models.Outbox)
+        self.outbox_manager = outbox_manager
+        self.outbox_model = outbox_model
+        
     def configure(self):
         print 'time to configure'
         self.start_action.setEnabled(True)
+        
+    def console(self):
+        print 'console'
 
     def start(self):
         print 'time to start'
         self.start_action.setEnabled(False)
         self.stop_action.setEnabled(True)
+        self.outbox_manager.start()
         
     def stop(self):
         print 'time to stop'
         self.start_action.setEnabled(True)
         self.stop_action.setEnabled(False)
-        
+        self.outbox_manager.terminate()
+        while not self.outbox_manager.is_terminated():
+            print 'waiting for outbox to terminate'
+            time.sleep(1)
+            
     def quit(self):
         print "quitin' time!"
         QtGui.qApp.quit()
 
 
 def main():
+    logging.basicConfig(level=logging.DEBUG)
+
     app = QtGui.QApplication(sys.argv)
+    app.setApplicationName('Tagfiler Outbox')
     QtGui.QApplication.setQuitOnLastWindowClosed(False)
     
     if QtGui.QSystemTrayIcon.isSystemTrayAvailable():
@@ -109,11 +136,14 @@ def main():
     default_path = os.path.join(os.path.expanduser('~'), 
                                 '.tagfiler', 'outbox.conf')
     (outbox_dao, outbox_model) = config.load_or_create_outbox(default_name, default_path)
+    state_dao = outbox_dao.get_state_dao(outbox_model)
+    outbox_manager = outbox.Outbox(outbox_model, state_dao)
     
     # Create the tray icon view and controller
-    controller = OutboxTrayIconController()
+    controller = OutboxTrayIconController(outbox_manager, outbox_model)
     trayicon = OutboxTrayIconView(QtGui.QIcon("tag.gif"))
     trayicon.configure_action.triggered.connect(controller.configure)
+    trayicon.console_action.triggered.connect(controller.console)
     trayicon.start_action.triggered.connect(controller.start)
     trayicon.stop_action.triggered.connect(controller.stop)
     trayicon.quit_action.triggered.connect(controller.quit)
