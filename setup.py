d4b75459bb4fbd1a529e632b27b927708ad7fc36
robes <robes@users.noreply.github.com>
2012-09-27 18:17:41 +0000

    complete setup instructions (for now)

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
 setup(name='tagfiler-iobox',
       version='0.1',
-      package_dir={'tagfiler': 'src/tagfiler'},
-      packages=['tagfiler', 'tagfiler.iobox', 'tagfiler.util', 'tagfiler.common'],
+      description='Tagfiler IOBox',
+      packages=['tagfiler', 'tagfiler.iobox', 'tagfiler.util', 'tagfiler.iobox.ui'],
+      package_dir={'': 'src'},
+      package_data={'tagfiler.iobox.ui': ['tag.gif'], 'tagfiler.iobox': ['sql/*.sql']},
+      scripts=['bin/tagfiler-outbox', 'bin/tagfiler-outbox-config', 'bin/tagfiler-outbox-systray']
       )
