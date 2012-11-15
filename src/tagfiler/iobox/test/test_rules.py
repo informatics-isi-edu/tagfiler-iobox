363710e1532c5a127e3a1fbdb97c3e1653b67bfd
robes <robes@users.noreply.github.com>
2012-11-15 21:45:33 +0000

    Update Rules unit tests

--- src/tagfiler/iobox/test/test_rules.py
+++ src/tagfiler/iobox/test/test_rules.py
@@ -1,16 +1,26 @@
-'''
-Created on Sep 19, 2012
+# 
+# Copyright 2010 University of Southern California
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-@author: smithd
-'''
-import unittest
-import logging
 from tagfiler.iobox.models import File
 from tagfiler.util.rules import PathRuleProcessor, TagDirector
-from tagfiler.iobox.test import base
 from tagfiler.iobox.test.base import create_date_and_study_path_rule
 from tagfiler.iobox.models import create_default_name_path_rule
 import socket
+import unittest
+import logging
 
 def all_tests():
     """Returns a TestSuite that includes all test cases in this module."""
@@ -37,18 +47,18 @@ class TestPathRuleProcessor(unittest.TestCase):
 class TestTagDirector(unittest.TestCase):
     def testRun(self):
         f = File()
-        f.set_filepath("/opt/data/studies/2012-02-23/session1/myfile.jpg")
+        f.filename = "/opt/data/studies/2012-02-23/session1/myfile.jpg"
         
         rules = [create_date_and_study_path_rule(), create_default_name_path_rule(socket.gethostname())]
         TagDirector().tag_registered_file(rules, f)
-        assert len(f.get_tags()) == 3
-        for t in f.get_tags():
-            if t.get_tag_name() == "date":
-                assert t.get_tag_value() == '2012-02-23'
-            elif t.get_tag_name() == "session":
-                assert t.get_tag_value() == "session1"
-            elif t.get_tag_name() == "name":
-                assert t.get_tag_value() == "file://%s/opt/data/studies/2012-02-23/session1/myfile.jpg" % socket.gethostname()
+        assert len(f.tags) == 3
+        for t in f.tags:
+            if t.name == "date":
+                assert t.value == '2012-02-23'
+            elif t.name == "session":
+                assert t.value == "session1"
+            elif t.name == "name":
+                assert t.value == "file://%s/opt/data/studies/2012-02-23/session1/myfile.jpg" % socket.gethostname()
             else:
                 assert False
 
