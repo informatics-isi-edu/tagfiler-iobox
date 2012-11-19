f8078c2b1982f67c99eab8ea9b144d21bb63c7e3
robes <robes@users.noreply.github.com>
2012-11-19 04:03:31 +0000

    Remove systray client

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 setup(name='tagfiler-iobox',
       version='0.1',
       description='Tagfiler IOBox',
-      packages=['tagfiler', 'tagfiler.iobox', 'tagfiler.util', 'tagfiler.iobox.ui'],
+      packages=['tagfiler', 'tagfiler.iobox', 'tagfiler.util'],
       package_dir={'': 'src'},
-      package_data={'tagfiler.iobox.ui': ['icons/*.gif'], 'tagfiler.iobox': ['sql/*.sql']},
-      scripts=['bin/tagfiler-outbox', 'bin/tagfiler-outbox-systray']
+      package_data={'tagfiler.iobox': ['sql/*.sql']},
+      scripts=['bin/tagfiler-outbox']
       )
